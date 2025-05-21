64066a70e8392dae1dbeed7311ea00e9b59f9f62
saibhargav4799 <saibhargavk47@gmail.com>
2025-05-21 18:17:21 +0530

    commit1

---
+++ src/main/java/org/example/corejavabrushup1.java
@@ -0,0 +1,93 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class corejavabrushup1 {
+
+
+    public static void main(String[] args) {
+
+//       int $mynum = 5;
+//       System.out.println($mynum);
+//
+//       short[] array = {1,2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8,12,1,2,12,1,2};
+//       System.out.println(Arrays.toString(array));
+//        System.out.println(array[0]+" "+" "+array[1]);
+
+//        int a=10,b=98;
+//        int sum = add(a,b);
+//        byte by=128;
+
+
+//        System.out.println("prints l"+l);
+
+//        int octal = 775;  // Octal input (as an int, no leading 0)
+//        int decimal = 0;
+//        int base = 1;
+//        int n =12321;
+//        int decimal2 = 0,base1=1;
+//
+//
+//        while (octal > 0) {
+//            int lastDigit = octal % 10;
+//            decimal += lastDigit * base;
+//            base = base * 8;
+//            octal = octal/ 10;
+//        }
+//
+//        System.out.println("Decimal value: " + decimal);
+
+//
+
+//        System.out.println("1"+2+3);
+//        System.out.println(1+2+"3");
+//        System.out.println(1+"2"+3);
+//
+//        int number = 121;  // Change this number as needed
+//        int originalNumber = number;
+//        String reversed ;
+//
+//        // Step-by-step: extract digits and build reversed number
+//        while (number > 0) {
+//            int digit = number % 10;               // Get last digit
+//            reversed = String.valueOf(digit);     // Concatenate digit
+//            number = number / 10;                  // Remove last digit
+//        }
+
+
+//        int[] x = {1, 2, 3, 4};
+//        int[] y;
+//        int[] z = new int[4];
+//
+//        y = new int[]{1,2,3};
+//        //print all numbers in array
+//        int n = x.length;
+//
+//        System.out.println(n);
+//        for(int i=0;i<n;i++){
+////            System.out.println(n);
+//            System.out.println(x[i]);
+//            z[i] = x[i];
+//
+//            System.out.println(Z[i]);
+        // Array
+        // array declaration
+//        int[] x;  //with out values
+//
+//        int[] y = {11,22,3,44,5,66};
+//
+////        x = new int[]{1,2,3,4,5,5,5};
+//        x = new int[5];
+
+
+//        int[] x = new int[3];
+//        int[] y = {1,2,3};
+//
+//        int [] z;
+//        z= y + new int[] {1,2,3};
+
+
+
+
+    }
+}
